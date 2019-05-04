65a9a0ee3ec1ed51626abbc7083b641fe6cb58b2
Sergio Gimeno <gimeno.sergio@gmail.com>
2019-05-04 12:07:51 +0200

    chore: create db file on first write request
     * return 404 status when collection does not exist in fs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json } = require('body-parser')
-const { load, save } = require('./state')
+const { load, save, create } = require('./state')
 
 const app = express()
 
@@ -14,12 +14,44 @@ app.use((req, res, next) => {
 })
 
 app.get('/:namespace', (req, res) => {
-  res.json(load(req.params.namespace))
+  let status, result
+  try {
+    result = load(req.params.namespace)
+    status = 200
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      status = 404
+    } else {
+      status = 500
+    }
+  } finally {
+    res = res.status(status)
+    if (result) {
+      res.json(result)
+    } else {
+      res.end()
+    }
+  }
 })
 
 app.post('/:namespace', json(), (req, res) => {
-  save(req.params.namespace, req.body)
-  res.json(load(req.params.namespace))
+  let status, result
+  try {
+    save(req.params.namespace, req.body)
+    result = load(req.params.namespace)
+    status = 200
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      create(req.params.namespace)
+      save(req.params.namespace, req.body)
+      result = load(req.params.namespace)
+      status = 200
+    } else {
+      status = 500
+    }
+  } finally {
+    res.status(status).json(result)
+  }
 })
 
 const server = app.listen(process.env.PORT, () => {
